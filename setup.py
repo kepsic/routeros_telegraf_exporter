34d6c3d217fb48c81b4a2e51761b45d5a3d5cc11
Andres Kepler <andres@kepler.ee>
2020-03-24 11:31:37 +0200

    Append missing deps

--- setup.py
+++ setup.py
@@ -10,7 +10,10 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = [ 'cornice',
+requirements = [ 'venusian', 
+                 'simplejson', 
+                 'pyramid',
+                 'cornice',
                  'waitress',
                  'routeros_api',
                  'pyyaml'
