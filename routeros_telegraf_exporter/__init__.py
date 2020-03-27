4a0b9094e8a329b0561e7a2c98e33a6750eb11f4
Andres Kepler <andres@kepler.ee>
2020-03-27 16:19:35 +0200

    Bump version: 0.1.8 → 0.1.9

--- routeros_telegraf_exporter/__init__.py
+++ routeros_telegraf_exporter/__init__.py
@@ -3,7 +3,7 @@
 
 __author__ = """Andres Kepler"""
 __email__ = 'andres@kepler.ee'
-__version__ = '0.1.8'
+__version__ = '0.1.9'
 
 import os
 import re
--- setup.py
+++ setup.py
@@ -66,6 +66,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/kepsic/{0}'.format(package_name),
-    version='0.1.8',
+    version='0.1.9',
     zip_safe=False,
 )
