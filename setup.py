23a3c2e6b21dd2aaa358fce2bb5af0b8130736aa
Andres Kepler <andres@kepler.ee>
2020-03-27 17:23:54 +0200

    Bump version: 0.1.9 → 0.1.10

--- setup.py
+++ setup.py
@@ -66,6 +66,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/kepsic/{0}'.format(package_name),
-    version='0.1.9',
+    version='0.1.10',
     zip_safe=False,
 )
